dfd2c9df9d5f7460b8d5f5d79e3420ae6b9ca697
Nicholas Beerbower <nbeerbower@gmail.com>
2023-09-26 11:02:20 -0400

    fix voicehandler

--- src/VoiceHandler.js
+++ src/VoiceHandler.js
@@ -3,15 +3,19 @@ const { spawn } = require('child_process');
 class VoiceHandler {
     constructor(options) {
         this.exe_location = options.exe_location;
-        this.output_device = options.output_device;
+        this.audio_device = options.audio_device;
         this.is_speaking = false;
     }
 
     speak(message) {
-        if (this.is_speaking) return;
+        if (this.is_speaking) {
+            console.warn(`WinTTS is already speaking`);
+            return;
+        }
         this.is_speaking = true;
         // spawn child process to speak message
-        const child = spawn(this.exe_location, [message, this.output_device]);
+        console.info(`WinTTS speaking: ${message}`);
+        const child = spawn(this.exe_location, [message, this.audio_device]);
         child.on('exit', (code, signal) => {
             console.info(`WinTTS exited`);
             this.is_speaking = false;
